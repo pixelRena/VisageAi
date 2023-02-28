272275b6fce5fb17db1130825b638a08663d608b
Serena Dean <dserena01@yahoo.com>
2023-02-28 17:20:01 -0600

    Update testing component

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import faceRecognition from "./components/FaceRecognition/faceRecognition";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Image Component', () => {
+  test('Response from clarifai api includes bounding_box', async () => {
+    const { container } = render(<faceRecognition/>);
+    const img = container.querySelector('img');
+    const res = await fetch(img.src);
+    const data = await res.json();
+    expect(data.outputs[0].data.regions[0].region_info.bounding_box).toBeDefined();
+  });
+})
+
