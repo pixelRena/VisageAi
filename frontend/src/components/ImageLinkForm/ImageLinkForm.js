0dfd2aecc1e5ec209eb6e1c830a14ead95e6dc33
Serena Dean <dserena01@yahoo.com>
2023-03-19 02:59:38 -0500

    Updated layout and added description of usage

--- frontend/src/components/ImageLinkForm/ImageLinkForm.js
+++ frontend/src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,20 +1,41 @@
 // This is the form on the homepage after logging in or registering
 // Where the user can enter a URL into the input field
 import React from 'react';
-import './ImageLinkForm.css';
+import Button from "react-bootstrap/button";
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from "react-bootstrap/Row";
 
 const ImageLinkForm = ({ onInputChange, onImageSubmit }) => {
 	return (
 		<div>
-			<p className='f3'>
-				{'Who\'s in your photo? Give us a image URL and we will detect faces for you.'}
-			</p>
-			<div className='center'>
-				<div className='form center pa4 br3 shadow-5'>
-					<input className='f4 pa2 w-100 center' type='tex' onChange={onInputChange}/>
-					<button className='detect-btn w-40 grow f4 link ph3 pv2 dib white ba ba--white bg-black' onClick={onImageSubmit}>{'Detect'}</button>
-				</div>
-			</div>
+			<h4>
+				Analyze Faces In Real-Time
+			</h4>
+			<Row className="text-start">
+				<p className="fs-6 fw-medium fst-italic form-text">How does it work?</p>
+			</Row>
+			<Row className="text-start">
+				<ol type="1">
+					<li>Find an image online</li>
+					<li>Copy the url of the image</li>
+					<li>Paste it into the input below</li>
+					<li>Then press Detect!</li>
+				</ol>
+				<p className="text-primary">Your image has been successfully analyzed to the left! Simple as pie.</p>
+			</Row>
+			<Row className='center'>
+				<InputGroup className='w-100 center'>
+					<Form.Control onChange={onInputChange} value="https://www.snexplores.org/wp-content/uploads/2022/08/1440_face_mites_feat.jpg"/>
+					<Button variant="primary" size="md" type="submit" onClick={onImageSubmit}>Detect</Button>
+				</InputGroup>
+			</Row>
+			<Row className="text-start pt-3">
+				<p className="fs-6 fw-medium fst-italic form-text">What if it doesn't work?</p>
+				<p>Make sure the url you pasted ends with a image file type such as: <span className="text-primary fw-bold">.jpg, .png, etc</span></p>
+				<p className="fs-6 fw-medium fst-italic form-text">Still not working?</p>
+				<p>Please submit a <a href="https://github.com/pixelRena/Clarifai-Model-Detection/issues" className="text-primary text-underline fw-bold">issue</a> request and the url you tried to use so that I can further troubleshoot. Thanks! 😊</p>
+			</Row>
 		</div>
 		);
 }
