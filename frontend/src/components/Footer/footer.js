108ba66cc688bba284552ce1c7a357bfb4fff363
Serena <serena.dean@autozone.com>
2023-10-19 12:54:14 -0500

    chore: adds aria-label to links

--- frontend/src/components/Footer/footer.js
+++ frontend/src/components/Footer/footer.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React from "react";
 import "./footer.css";
 
 const Footer = () => {
-	return (
-		<div id='footer'>
-			<p className="text-uppercase link">© <a 
-				className="text-dark"
-				target="_blank"
-				rel="noopener noreferrer"
-				href='https://github.com/pixelRena/Clarifai-Model-Detection'
-				style={{'textDecoration':'none'}}>
-				pixelRena/@github</a> 
-			<script>document.write(new Date().getFullYear())</script>
-			</p>
-		</div>
-		);
-}
+  return (
+    <div id="footer">
+      <p className="text-uppercase link">
+        ©{" "}
+        <a
+          className="text-dark"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/pixelRena/Clarifai-Model-Detection"
+          style={{ textDecoration: "none" }}
+          aria-label="Go to pixelRena's github to view VisageAi repository"
+        >
+          pixelRena/@github
+        </a>
+        <script>document.write(new Date().getFullYear())</script>
+      </p>
+    </div>
+  );
+};
 
 export default Footer;
--- frontend/src/components/ImageLinkForm/ImageLinkForm.js
+++ frontend/src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,44 +1,76 @@
 // This is the form on the homepage after logging in or registering
 // Where the user can enter a URL into the input field
-import React from 'react';
+import React from "react";
 import Button from "react-bootstrap/Button";
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
 import Row from "react-bootstrap/Row";
 
 const ImageLinkForm = ({ onInputChange, onImageSubmit }) => {
-	return (
-		<div>
-			<h4>
-				Analyze Faces In Real-Time
-			</h4>
-			<Row className="text-start">
-				<p className="fs-6 fw-medium fst-italic form-text">How does it work?</p>
-			</Row>
-			<Row className="text-start">
-				<ol type="1">
-					<li>Find an image online</li>
-					<li>Copy the url of the image</li>
-					<li>Paste it into the input below</li>
-					<li>Then press Detect!</li>
-				</ol>
-				<p className="text-primary">Your image has been successfully analyzed to the left! Simple as pie.</p>
-			</Row>
-			<Row className='center'>
-				<InputGroup className='w-100 center'>
-					<Form.Control onChange={onInputChange}/>
-					<Button variant="primary" size="md" type="submit" onClick={onImageSubmit}>Detect</Button>
-				</InputGroup>
-			</Row>
-			<Row className="text-start pt-3">
-				<p className="fs-6 fw-medium fst-italic form-text">What if it doesn't work?</p>
-				<p>Make sure the url you pasted ends with a image file type such as: <span className="text-primary fw-bold">.jpg, .png, etc</span></p>
-				<p className="fs-6 fw-medium fst-italic form-text">Still not working?</p>
-				<p>Please submit a <a href="https://github.com/pixelRena/Clarifai-Model-Detection/issues" className="text-primary text-underline fw-bold">issue</a> request and the url you tried to use so that I can further troubleshoot. Thanks! 😊</p>
-				<a href="https://status.clarifai.com/" target="_blank" rel="noopener noreferrer" className="text-center">Clarifai Server Status</a>
-			</Row>
-		</div>
-		);
-}
+  return (
+    <div>
+      <h4>Analyze Faces In Real-Time</h4>
+      <Row className="text-start">
+        <p className="fs-6 fw-medium fst-italic form-text">How does it work?</p>
+      </Row>
+      <Row className="text-start">
+        <ol type="1">
+          <li>Find an image online</li>
+          <li>Copy the url of the image</li>
+          <li>Paste it into the input below</li>
+          <li>Then press Detect!</li>
+        </ol>
+        <p className="text-primary">
+          Your image has been successfully analyzed to the left! Simple as pie.
+        </p>
+      </Row>
+      <Row className="center">
+        <InputGroup className="w-100 center">
+          <Form.Control onChange={onInputChange} />
+          <Button
+            variant="primary"
+            size="md"
+            type="submit"
+            onClick={onImageSubmit}
+          >
+            Detect
+          </Button>
+        </InputGroup>
+      </Row>
+      <Row className="text-start pt-3">
+        <p className="fs-6 fw-medium fst-italic form-text">
+          What if it doesn't work?
+        </p>
+        <p>
+          Make sure the url you pasted ends with a image file type such as:{" "}
+          <span className="text-primary fw-bold">.jpg, .png, etc</span>
+        </p>
+        <p className="fs-6 fw-medium fst-italic form-text">
+          Still not working?
+        </p>
+        <p>
+          Please submit a{" "}
+          <a
+            href="https://github.com/pixelRena/Clarifai-Model-Detection/issues"
+            className="text-primary text-underline fw-bold"
+          >
+            issue
+          </a>{" "}
+          request and the url you tried to use so that I can further
+          troubleshoot. Thanks! 😊
+        </p>
+        <a
+          href="https://status.clarifai.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Go to Clarifai's website to verify server status"
+          className="text-center"
+        >
+          Clarifai Server Status
+        </a>
+      </Row>
+    </div>
+  );
+};
 
 export default ImageLinkForm;
